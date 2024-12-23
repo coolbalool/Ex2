9584469db7f8e6ab3d31227a6f6b9eadf984d71a
Itamar <itamarca117@gmail.com>
2024-12-23 14:30:20 +0200

    need to fix is Form finished is number and isText

--- src/Cell.java
+++ src/Cell.java
@@ -28,6 +28,32 @@ public class Cell
         return (dotCount < 2); // true if count is 0 or 1 else false
     }
 
+    public static boolean isText(String text)
+    {
+        return (!isNumber(text) && !isForm(text));
+    }
+
+    public static boolean isForm(String text)
+    {
+        char ch = text.charAt(0);
+        StringBuilder currNum = new StringBuilder();
+        if (ch != '=') return false;
+        ch = text.charAt(1);
+        if (ch != '-' &&  (ch <'0' || ch > '9') && ch != '+') return false;
+        currNum.append(ch);
+        for (int i = 2; i < text.length(); i++)
+        {
+            ch = text.charAt(i);
+            if ((ch >='0' && ch <= '9' )|| ch == '.') currNum.append(ch);
+            else if ()
+        }
+
+
+    }
+
+
+
+
 
 
 }
--- src/TestEx2.java
+++ src/TestEx2.java
@@ -1,12 +1,23 @@
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 public class TestEx2
 {
 
     @Test
-    public static void TestIsNumber()
+    public void TestIsNumber()
     {
         assertTrue(Cell.isNumber("+3.55"));
+        assertTrue(Cell.isNumber("-3.55"));
+        assertTrue(Cell.isNumber("999"));
+        assertTrue(Cell.isNumber("+0095"));
+
+        assertFalse(Cell.isNumber("34.4.4"));
+        assertFalse(Cell.isNumber("4-2"));
+        assertFalse(Cell.isNumber("[23p4"));
+        assertFalse(Cell.isNumber("34.4-4"));
+
 
 
     }
